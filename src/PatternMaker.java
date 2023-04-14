76df2760f2783a76ae066934d9dd9b8e3b5f9eb9
tadeyefa <ohuntoluwase_adeyefa25@milton.edu>
2023-04-13 20:16:53 -0400

    Added pattern 4 and attempted to do pattern 6

--- src/PatternMaker.java
+++ src/PatternMaker.java
@@ -7,7 +7,9 @@ public class PatternMaker {
 //        pattern1();
 //        pattern2();
 //        pattern3();
-        pattern5();
+//        pattern4();
+//        pattern5();
+        pattern6();
     }
     public void pattern1() {
         for (int x=0;x<6;x++) {
@@ -24,12 +26,22 @@ public class PatternMaker {
             System.out.print(21 - (a * 4) + "\t");
         }
     }
+    public void pattern4() {
+        for (int w=1;w<7;w++) {
+            System.out.print(w * w + "\t");
+        }
+    }
     public void pattern5() {
         for (int b=1;b<5;b++) {
             System.out.println();
             for (int c=1;c<5;c++) {
                 System.out.print(c);
             }
+        }
+    }
+    public void pattern6() {
+        for (int v=1;v<6;v++) {
+            System.out.println(" " + v);
         }
     }
 }
